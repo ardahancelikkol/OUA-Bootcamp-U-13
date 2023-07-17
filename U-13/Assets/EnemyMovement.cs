3b3ed4161d818021e9a66acda7d015ec34ba62f9
HasnunCoskun <104356729+HasnunCoskun@users.noreply.github.com>
2023-07-17 22:11:30 +0300

    enemy attack

--- U-13/Assets/EnemyMovement.cs
+++ U-13/Assets/EnemyMovement.cs
@@ -6,15 +6,21 @@ public class EnemyMovement : MonoBehaviour
 {
 
     public Transform PlayerTF;
+    public PlayerController player;
+    
     public float hspeed = 20;
     public float maxspeed = 10;
     public float seeRange = 10;
-    
+    public float attackRange = 0.7f;
+    public float attackInterval = 2f;
+    public float damage = 5;
+
+    private float attackTimer = 0;
     private float distance;
     private float side;
     private Rigidbody2D rb2d;
     private Animator animator;
-    private float scaleside;
+    private bool canAttack;
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +32,28 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         distance = transform.position.x - PlayerTF.position.x;
         side = Mathf.Sign(distance);
+        canAttack = attackTimer < 0;
+        animator.SetBool("canAttack", canAttack);
 
         transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * side, transform.localScale.y, transform.localScale.z);
 
-        if (Mathf.Abs(distance) < seeRange && rb2d.velocity.x < maxspeed)
+        if (Mathf.Abs(distance) < seeRange && Mathf.Abs(distance) > attackRange)
         {
             EnemyMove(side);
+        }
+        else if(Mathf.Abs(distance) < attackRange && canAttack)
+        {
+            EnemyAttack();
         }
         else
         {
             animator.SetBool("canWalk", false);
         }
+
+        attackTimer -= Time.deltaTime;
     }
 
     private void EnemyMove(float direction)
@@ -46,4 +61,15 @@ public class EnemyMovement : MonoBehaviour
         transform.position = new Vector2(transform.position.x - (hspeed * direction / 1000), transform.position.y);
         animator.SetBool("canWalk", true);
     }
+
+    private void EnemyAttack()
+    {
+        attackTimer = attackInterval;
+        animator.SetTrigger("Attack");
+    }
+
+    public void DamagePlayer()
+    {
+        player.TakeDamage(damage);
+    } 
 }
