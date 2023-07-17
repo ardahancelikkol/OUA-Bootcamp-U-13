9b5fd7279edccca3b8d46fc9428029456df8d49d
HasnunCoskun <104356729+HasnunCoskun@users.noreply.github.com>
2023-07-17 18:03:44 +0300

    Revert "Camera"
    This reverts commit cef4b65b5a3f3f81abf0e73c885900ca02fe6e23.

--- U-13/Assets/King.cs
+++ U-13/Assets/King.cs
@@ -5,61 +5,22 @@ using TMPro;
 
 public class King : MonoBehaviour {
 
-    public DialogueManager dialogueManager;
     public TextMeshProUGUI dialogueTMP;
-    public LayerMask playerLayer;
-    public GameObject dialogueBox;
-
-
-    [TextArea(3,10)] public string[] diags1;
-
-    private int diagCount = 0;
-    private string[] currentDiags;
-    private Transform kingTransform;
-    private bool dialogueActive;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        kingTransform = GetComponent<Transform>();
-
-        currentDiags = diags1;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        dialogueBox.SetActive(IsPlayerNear() && dialogueActive == false);
-
-
-        if (IsPlayerNear() == false)
-        {
-            dialogueManager.EndDialogue();
-        }
-
-        if (Input.GetKeyDown(KeyCode.E) && IsPlayerNear())
-        {
-            if (diagCount == currentDiags.Length)
-            {
-                dialogueActive = false;
-                dialogueManager.EndDialogue();
-                diagCount = 0;
-            }
-            else
-            {
-                dialogueManager.ShowDialogue(currentDiags[diagCount]);
-                dialogueActive = true;
-                diagCount++;
-            }
-        }
-
+        
     }
 
-    private bool IsPlayerNear()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        return Physics2D.Raycast(transform.position, Vector2.left, 2f, playerLayer);
+        Debug.Log("Enter collision!");
     }
 }
