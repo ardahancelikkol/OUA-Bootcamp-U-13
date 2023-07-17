b15debb9d5726314ed1464351f96c86e037390c7
HasnunCoskun <104356729+HasnunCoskun@users.noreply.github.com>
2023-07-17 22:58:20 +0300

    HitBox

--- U-13/Assets/HitBox.cs
+++ U-13/Assets/HitBox.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class HitBox : MonoBehaviour
 {
     public bool HitPlayer = false;
@@ -11,8 +9,9 @@ public class HitBox : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == PlayerLayer)
+        if (collision.gameObject.layer == 6)
         {
+            Debug.Log("Hit Player!" + collision.gameObject.layer);
             HitPlayer = true;
         }
     }
