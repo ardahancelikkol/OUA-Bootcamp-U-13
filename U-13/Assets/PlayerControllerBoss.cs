d8fd7036db60f59ddcea22d3d58c3ecf3f346749
HasnunCoskun <104356729+HasnunCoskun@users.noreply.github.com>
2023-07-18 10:18:04 +0300

    ben bi pushlayayım bakalım

--- U-13/Assets/PlayerControllerBoss.cs
+++ U-13/Assets/PlayerControllerBoss.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerControllerBoss : MonoBehaviour
 {
     public Transform attackPos;
+    public LayerMask enemyLayer;
     public LayerMask groundLayer;
     public LayerMask bossLayer;
     public RectTransform healthBar;
@@ -18,19 +21,18 @@ public class PlayerControllerBoss : MonoBehaviour
     public float hspeed;
     public float maxspeed = 10;
     public float jumpForce;
-    public float MaxHealth = 100;
-    
-    [Range(0, 100)]
     public float Health;
-    
-    [Range(0,100)]
+    public float MaxHealth = 100;
+
+    [Range(0, 100)]
     public float Stress;
-    
-    public float Anxiety = 3;
-    public float attackRange = 5;
+
+    public float Anxiety;
+    public float attackRange;
     public float attackInterval = 0.3f;
-    public float pDamage = 5;
-    public float red_duration = 0.5f;
+    public int attackLandCount = 7;
+    public float pDamage;
+    public float red_duration;
     public float minAttackChance = 10;
     public float maxAttackChance = 90;
     public float maxStress = 100;
@@ -39,7 +41,9 @@ public class PlayerControllerBoss : MonoBehaviour
     public int deathCount = 0;
     public bool Alive = true;
 
+    private Collider2D[] enemiesToDamage;
     private Collider2D[] bossesToDamage;
+    private int attackLandTimer = -1;
     private float stress_timer;
     private float attackTimer;
     private bool isJump;
@@ -52,14 +56,14 @@ public class PlayerControllerBoss : MonoBehaviour
 
     System.Random rnd = new System.Random();
 
+
     private void Awake()
     {
         Application.targetFrameRate = 30;
     }
-
-    // Start is called before the first frame update
     void Start()
     {
+
         Health = MaxHealth;
         Stress = PlayerPrefs.GetFloat("Stress");
 
@@ -68,13 +72,25 @@ public class PlayerControllerBoss : MonoBehaviour
         animator = gameObject.GetComponent<Animator>();
         sprite_renderer = gameObject.GetComponent<SpriteRenderer>();
 
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayer);
         bossesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, bossLayer);
-        canHitEnemy = bossesToDamage.Length > 0;
+        bossesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, bossLayer);
+        canHitEnemy = enemiesToDamage.Length > 0;
+
+
+        if (PanicMode)
+        {
+            attackChance = 100f;
+        }
+        else
+        {
+            attackChance = maxAttackChance - (Stress * ((maxAttackChance - minAttackChance) / maxStress));
+        }
 
         hmove = Input.GetAxisRaw("Horizontal");
         isJump = Input.GetKeyDown(KeyCode.Space);
@@ -86,22 +102,39 @@ public class PlayerControllerBoss : MonoBehaviour
         animator.SetBool("isPanic", PanicMode);
         animator.SetBool("isGrounded", Grounded);
 
+        ///////////////////////////////////
+
         if (hmove != 0) { transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * hmove, transform.localScale.y, 0); }
+
         if (isJump && Grounded && Alive) { rb.velocity = Vector2.up * jumpForce; }
 
-        if (PanicMode)
-        {
-            attackChance = 100f;
-        }
-        else
-        {
-            attackChance = maxAttackChance - (Stress * ((maxAttackChance - minAttackChance) / maxStress));
-        }
+
 
         if (Input.GetMouseButtonDown(0) && (attackTimer <= 0) && Alive)
         {
-            Attack();
-        }
+            attackTimer = attackInterval;
+            attackLandTimer = attackLandCount;
+            animator.SetTrigger("playAttack");
+
+            foreach (Collider2D enemy in enemiesToDamage)
+            {
+                enemy.GetComponent<EnemyMovement>().TakeDamage(pDamage * (1 + Stress * 0.12f));
+            }
+
+            foreach (Collider2D boss in bossesToDamage)
+            {
+                boss.GetComponent<BossController>().TakeDamage(pDamage * (1 + Stress * 0.12f));
+
+            }
+            if (PanicMode)
+            {
+                Health += pDamage;
+                Stress -= Anxiety;
+            }
+
+        }//when the attack is started
+ //when the attack hits (can land or miss)
+
 
         if (red_timer > 0)
         {
@@ -111,6 +144,10 @@ public class PlayerControllerBoss : MonoBehaviour
         {
             sprite_renderer.color = Color.white;
         }
+
+        ////////////////////////////////
+
+        //PANIC MODE
 
         if (Stress >= maxStress && PanicMode == false)
         {
@@ -138,27 +175,19 @@ public class PlayerControllerBoss : MonoBehaviour
 
         attackTimer -= Time.deltaTime;
         red_timer -= Time.deltaTime;
+        attackLandTimer -= 1;
 
 
         if (Stress >= maxStress) { Stress = maxStress; }
         if (Stress <= 0) { Stress = 0; }
         if (Health >= MaxHealth) { Health = MaxHealth; }
         if (Health <= 0) { Health = 0; }
-
         if (Health <= 0 && Alive)
         {
             Die();
+            Alive = false;
         }
 
-
-
-    }
-
-    private void LateUpdate()
-    {
-
-        healthBar.localScale = new Vector3(Health / MaxHealth, healthBar.localScale.y, healthBar.localScale.z);
-        stressBar.localScale = new Vector3(Stress / maxStress, healthBar.localScale.y, healthBar.localScale.z);
     }
 
     void FixedUpdate()
@@ -177,11 +206,19 @@ public class PlayerControllerBoss : MonoBehaviour
 
     }
 
-    private bool IsGrounded()
+    void OnDrawGizmosSelected()
     {
-        return Physics2D.Raycast(collider2d.bounds.center, Vector2.down, collider2d.bounds.extents.y + 1f, groundLayer);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
-    
+
+    private void LateUpdate()
+    {
+
+        healthBar.localScale = new Vector3(Health / MaxHealth, healthBar.localScale.y, healthBar.localScale.z);
+        stressBar.localScale = new Vector3(Stress / maxStress, healthBar.localScale.y, healthBar.localScale.z);
+    }
+
     public void TakeDamage(float damage)
     {
         if (red_timer <= 0)
@@ -193,42 +230,35 @@ public class PlayerControllerBoss : MonoBehaviour
 
     }
 
-    /////////////////////////////
-
-
-    private void Attack()
+    public void TakeStress()
     {
-        attackTimer = attackInterval;
-        animator.SetTrigger("playAttack");
+        if (stress_timer < 0)
+            stress_timer = stress_duration;
+        Stress += Anxiety;
     }
 
-    private void HitAttack()
+    public void Attack()
     {
-        if(canHitEnemy)
+        foreach (Collider2D enemy in enemiesToDamage)
         {
-            if (rnd.Next(0, 100) <= attackChance)
-            {
-                LandAttack();
-            }
-            else
-            {
-                TakeStress();
-            }
+            enemy.GetComponent<EnemyMovement>().TakeDamage(pDamage * (1 + Stress * 0.12f));
         }
-    }
 
-    private void LandAttack()
-    {
-        foreach(Collider2D boss in bossesToDamage)
+        foreach (Collider2D boss in bossesToDamage)
         {
             boss.GetComponent<BossController>().TakeDamage(pDamage * (1 + Stress * 0.12f));
+
         }
+        if (PanicMode)
+        {
+            Health += pDamage;
+            Stress -= Anxiety;
+        }
+    }//Allows the player to attack each enemy seperately
 
-    }
-
-    private void TakeStress()
+    private bool IsGrounded()
     {
-        Stress += Anxiety;
+        return Physics2D.Raycast(collider2d.bounds.center, Vector2.down, collider2d.bounds.extents.y + 1f, groundLayer);
     }
 
     private void Die()
@@ -237,5 +267,12 @@ public class PlayerControllerBoss : MonoBehaviour
         hspeed = 0;
         jumpForce = 0;
         Alive = false;
+        StartCoroutine("GameOver");
+    }
+
+    private IEnumerator GameOver()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameOver");
     }
 }
